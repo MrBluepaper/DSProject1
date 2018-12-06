bc0c684a808eb84a9b372da90a13b88c8c3d47bf
Bluepaper <alirezagh110@gmail.com>
2018-12-06 11:15:22 +0330

    reading from files placed in main and compiled

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-#include "BookClass.h"
 #include "ReadFile.h"
 using namespace std;
 int main() {
   BookClass *head = new BookClass;
   readFile r(head);
-  while(head->getNextPtr())
+  r.readFileFunction();
+  BookClass *tmp = head;
+  while(tmp){
     head->printBookDetails();
+    tmp = tmp->getNextPtr();
+  }
 }
