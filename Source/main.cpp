d44532013b74ccf12e91f0c0b7ea23631726df45
Bluepaper <alirezagh110@gmail.com>
2018-12-06 13:31:02 +0330

    print commands added

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,6 +2,9 @@
 #include "ReadFile.h"
 #include "linkedListFunctions.h"
 using namespace std;
+
+int printCommands();
+
 int main() {
   BookClass *head = new BookClass;
   readFile r(head);
@@ -15,4 +18,23 @@ int main() {
     tmp = tmp->getNextPtr();
   }
   while(tmp != head);*/
+  printCommands();
+}
+
+int printCommands(){
+  int a;
+  cout << "====<< Commands >>====\n"
+        << "1. Print Book list\n"
+        << "2. Add new Book\n"
+        << "3. Add new Customer\n"
+        << "4. Exit Customer\n"
+        << "5. Customer Management\n"
+        << "======================\n\n"
+        << "Enter your command number (1-5): ";
+  cin >> a;
+  while (a > 5 || a < 1){
+    cout << "Wrong input;\nEnter it again : ";
+    cin >> a;
+  }
+  return a;
 }
