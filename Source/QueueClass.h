a73f63a0345868bb74e4f69ebfd570e3a9c98529
Bluepaper <alirezagh110@gmail.com>
2018-12-06 17:01:18 +0330

    Customer's name otion added

--- Source/QueueClass.h
+++ Source/QueueClass.h
@@ -2,10 +2,13 @@
 class QueueClass{
 private:
   bool sex; //true --> male         false --> female
+  string name;
   ShopClass shopping;
   QueueClass *nextPtr;
 public:
   QueueClass(){nextPtr = nullptr;}
+  void setName(string s){name = s;}
+  string getName(){return name;}
   void setSex(bool a){sex = a;}
   bool getSex(){return sex;}
   ShopClass *getShop(){return &shopping;}
