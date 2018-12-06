e7630de5d993ff3f68a31969aad7bf32a39693f6
Bluepaper <alirezagh110@gmail.com>
2018-12-06 12:43:54 +0330

    delete a book added to functions

--- Source/linkedListFunctions.h
+++ Source/linkedListFunctions.h
@@ -10,7 +10,30 @@ void add_l(BookClass *head, BookClass *new_a){
 }
 
 BookClass *delete_l(BookClass *head, string bookName){
-
+  BookClass *tmp = head;
+  if(head->getNextPtr() == head){
+    if(head->getBookName() == bookName){
+      delete head;
+      return nullptr;
+    }
+    else{
+      cout << "Book not found to delete!\n";
+      return head;
+    }
+  }
+  else{
+    do {
+      if(tmp->getNextPtr()->getBookName() == bookName){
+        BookClass *y = tmp->getNextPtr()->getNextPtr();
+        delete tmp->getNextPtr();
+        tmp->setNextPtr(y);
+        return head;
+      }
+      tmp = tmp->getNextPtr();
+    } while(head != tmp);
+    cout << "Book not found to delete!\n";
+    return head;
+  }
 }
 
 BookClass *search_l(BookClass *head, string bookName){
