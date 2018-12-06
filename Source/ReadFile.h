a01302e73b9057e2255f896d255ea61a73f04b78
Bluepaper <alirezagh110@gmail.com>
2018-12-06 10:51:26 +0330

    ReadFile.cpp created

--- Source/ReadFile.h
+++ Source/ReadFile.h
@@ -12,29 +12,3 @@ public:
   readFile(BookClass *head);
   bool readFileFunction();
 };
-readFile::readFile(BookClass *head){
-  this->head = head;
-}
-bool readFile::readFileFunction(){
-  ifstream bookFile("bookName.txt");
-  if(!bookFile.is_open()){
-    cerr << "Unable to open file :" << '\n';
-    return false;
-  }
-  string line, word;
-  while (getline(bookFile,line)) {
-    stringstream s(line);
-    s >> word;
-    head->setPubYear(atoi(word.c_str()));
-    s >> word;
-    head->setBookName(word);
-    s >> word;
-    head->setWriter(word);
-    s >> word;
-    head->setPrice(atoi(word.c_str()));
-    BookClass *tmp = new BookClass;
-    head->setNextPtr(tmp);
-    head = tmp;
-  }
-  return true;
-}
