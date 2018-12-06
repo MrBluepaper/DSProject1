e2864e6df61a963472ccf684e67d3aa288111092
Bluepaper <alirezagh110@gmail.com>
2018-12-06 16:16:02 +0330

    ShopClass for books for clients completed

---
+++ Source/ShopClass.h
@@ -0,0 +1,48 @@
+class ShopClass{
+private:
+  string *books;
+  double price;
+  int size;
+public:
+  ShopClass(){size = 0; books = nullptr; price = 0;}
+  void setSize(int a){size = a;}
+  int getSize(){return size;}
+  void addBook(string bookN){
+    size++;
+    if(books == nullptr){
+      books = new string[1];
+      books[0] = bookN;
+      return;
+    }
+    else{
+      string *tmp = new string[size];
+      for(int i = 0; i < size - 1; i++)
+        tmp[i] = books[i];
+      tmp[size - 1] = bookN;
+      delete[] books;
+      books = tmp;
+    }
+  }
+  string deleteBook(){
+    if(books == nullptr){
+      cout << "No Book.\n";
+      return 0;
+    }
+    else{
+      size--;
+      string *tmp = new string[size];
+      for(int i = 0; i < size; i++){
+        tmp[i] = books[i];
+        string u = books[size];
+        delete[] books;
+        books = tmp;
+        return u;
+      }
+    }
+  }
+  string *getBooks(){return books;}
+  void setPrice(double d){
+    price += d;
+  }
+  double getPrice(){return price;}
+};
